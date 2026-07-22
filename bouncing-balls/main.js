f70a53c4e85cf0a478d2e2fcbe838f1f29872187
Alejandro Madriñán Fernández <36339608+alexmadrinanfdez@users.noreply.github.com>
2026-07-22 11:03:51 -0500

    bouncing balls - more compact code
    - Reduce comments
    - Join conditions
    - Etc.

--- bouncing-balls/main.js
+++ bouncing-balls/main.js
@@ -1,16 +1,13 @@
-// setup canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
@@ -22,10 +19,9 @@ class Ball {
     this.velX = velX;
     this.velY = velY;
     this.color = color;
-    this.size = size; // radius, in pixels
+    this.size = size;
   }
   
-  // method to draw the ball on the canvas
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -33,53 +29,38 @@ class Ball {
     ctx.fill();
   }
 
-  // method to move the ball
   update() {
-    // check for collision with the walls
-    if (this.x + this.size >= width) {
+    if (this.x + this.size >= width || this.x - this.size <= 0) {
       this.velX = -this.velX;
     }
 
-    if (this.x - this.size <= 0) {
-      this.velX = -this.velX;
-    }
-
-    if (this.y + this.size >= height) {
-      this.velY = -this.velY;
-    }
-
-    if (this.y - this.size <= 0) {
+    if (this.y + this.size >= height || this.y - this.size <= 0) {
       this.velY = -this.velY;
     }
     
-    // update position
     this.x += this.velX;
     this.y += this.velY;
   }
 
-  // method to detect collision with other balls
   collisionDetect() {
     for (const ball of balls) {
       if (this !== ball) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+        if (Math.hypot(dx, dy) < this.size + ball.size) {
+          this.color = ball.color = randomRGB();
         }
       }
     }
   }
 }
 
-// create and store the balls
 const balls = [];
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
+    // ball position drawn at least one ball width from the edge of the canvas
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
@@ -91,10 +72,9 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
-// animation loop
-function loop() {
-  // cover the previous frame with a semi-transparent color to create a fading trail effect
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+(function loop() {
+  // cover last frame with a semi-transparent color to create a fading trail effect
+  ctx.fillStyle = "rgb(0 0 0 / 35%)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
@@ -104,6 +84,4 @@ function loop() {
   }
 
   requestAnimationFrame(loop);
-}
-
-loop();
+})();
